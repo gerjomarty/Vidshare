1e7b6047104801db29fcb91902209c5ec2cc9fc0
Gerard Martin <gerard.martin@cantab.net>
2010-02-11 00:41:00 +0000

    Fleshing out VideoStream class. Now need to implement Lollipop numbering to use for dObj numbering. Continue next time in VideoStream.
    git-svn-id: file:///home/gerard/programming/vidshare-svn/trunk@25 3e7b97c2-2e4e-4883-83ef-b2c1530b9b69

--- VidshareHaggle/src/com/gm375/vidshare/VideoStream.java
+++ VidshareHaggle/src/com/gm375/vidshare/VideoStream.java
@@ -1,7 +1,103 @@
 package com.gm375.vidshare;
 
-import android.app.Activity;
+import java.io.File;
 
-public class VideoStream extends Activity {
+import android.app.Activity;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.view.OrientationEventListener;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class VideoStream extends Activity implements View.OnClickListener, SurfaceHolder.Callback {
+    
+    private boolean mIsPreviewing = false;
+    private VideoPreview mVideoPreview;
+    private MediaRecorder mMediaRecorder;
+    private int mSavedWidth, mSavedHeight;
+    OrientationEventListener mOrientationListener;
+    private android.hardware.Camera mCamera;
+    private android.hardware.Camera.Parameters mParameters;
+    private ImageView mShutterButton;
+    private SurfaceHolder mSurfaceHolder = null;
+    
+    int mLastOrientation = OrientationEventListener.ORIENTATION_UNKNOWN;
+    
+    //File dir;
+    //String filepath;
+    
+    private static final int VIDEO_FRAME_RATE = 15;
+    
+    private static final int STATUS_IDLE = 0;
+    private static final int STATUS_STREAMING_VIDEO = 1;
+    private static final int STATUS_STOPPING_STREAM = 2;
+    
+    private int mStatus = STATUS_IDLE;
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        Window win = getWindow();
+        win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
+        setContentView(R.layout.video_record);
+        
+        mVideoPreview = (VideoPreview) findViewById(R.id.video_record_surface);
+        
+        SurfaceHolder holder = mVideoPreview.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        
+        mShutterButton = (ImageView) findViewById(R.id.shutter_button);
+        mShutterButton.setOnClickListener(this);
+    }
+    
+    
+    public MediaRecorder createMediaRecorder(String filename) {
+        MediaRecorder mr = new MediaRecorder();
+        mr.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        mr.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mr.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mr.setVideoEncoder(MediaRecorder.VideoEncoder.H263);
+        mr.setOutputFile(filename);
+        mr.setVideoSize(mSavedWidth, mSavedHeight);
+        mr.setVideoFrameRate(VIDEO_FRAME_RATE);
+        mr.setPreviewDisplay(mSurfaceHolder.getSurface());
+        return mr;
+    }
+    
+    // TODO: START HERE 11/02/2010: Need to create Util class with Lollipop numbering scheme and associated methods.
+    // Continue fleshing out this class using VideoRecord as a template.
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+            int height) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
 
 }
